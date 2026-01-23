f9449d6ca9f96c854582c8f287c069d26ce033ec
Doha choukri <devnine16@gmail.com>
2026-01-23 12:16:22 +0100

    init project repository

---
+++ database/seeders/PromoLineDetailSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PromoLineDetailSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}

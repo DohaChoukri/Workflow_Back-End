a2958ae7434a41ad21b73db828eaa73a089194b7
Doha choukri <devnine16@gmail.com>
2026-01-26 16:29:54 +0100

    creation la partie de connexion

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,30 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Manager User',
+            'email' => 'manager@example.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Regular User',
+            'email' => 'user@example.com',
+        ]);
+
+        $this->call([
+            // PermissionsSeeder::class,
+            RolesSeeder::class,
+            RolePermissionSeeder::class,
+            UserRoleSeeder::class,
+            ClientSeeder::class,
+            ProduitSeeder::class,
+            // PromoSeeder::class,
+            // PromoLineDetailSeeder::class,
+            // PersonnalAccessTokensSeeder::class,
         ]);
     }
 }
